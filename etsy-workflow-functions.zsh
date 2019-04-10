53240c1828cee715485416c4be8e30be59c46582
Josh Stewart <jstewart@etsy.com>
2019-04-10 09:46:07 -0400

    Add function for is clean and is master

--- etsy-workflow-functions.zsh
+++ etsy-workflow-functions.zsh
@@ -1,3 +1,29 @@
-function wf_test() {
-    echo "wf_test"
+export VM_HOST=jstewart.vm.dev.etsycloud.com
+
+function wf_is_branch_clean() {
+    if [[ -z $(git status -s) ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+function wf_is_branch_master() {
+    branch=$(git rev-parse --abbrev-ref HEAD);
+    if [[ $branch = 'master' ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+function wf_new_branch() {
+    if [ "$#" -ne 1 ]; then
+        echo "Create a new branch on both the VM and locally. Usage: $0 [branch-name]"
+        return 1
+    fi
+
+    echo "Making branch $1"
+    git checkout -b $1
+    git push -u origin HEAD
 }
