239d23062109e84deeed2568e4a420101821c90a
Josh Stewart <jstewart@etsy.com>
2019-04-10 10:37:58 -0400

    Update VM after creating branch

--- etsy-workflow-functions.zsh
+++ etsy-workflow-functions.zsh
@@ -4,7 +4,7 @@ function wf_is_branch_clean() {
     if [[ -z $(git status -s) ]]; then
         return 0
     else
-        echo "branch not clean"
+        echo "Error: branch not clean"
         return 1
     fi
 }
@@ -14,8 +14,26 @@ function wf_is_branch_master() {
     if [[ $branch = 'master' ]]; then
         return 0
     else
-        echo "branch not master"
+        echo "Error: branch not master"
         return 1
+    fi
+}
+
+function wf_checkout_branch() {
+    if [ "$#" -ne 1 ]; then
+        echo "Check out branch. Usage: $0 [branch-name]"
+        return 1
+    fi
+
+    echo "Checking out branch $1"
+
+    git fetch
+    git checkout $1
+    if [ $? -ne 0]; then
+        echo "Error: check out branch"
+        return 1
+    else
+        return 0
     fi
 }
 
@@ -38,7 +56,10 @@ function wf_new_branch() {
         return 1
     fi
 
+    git pull -r
     git checkout -b $1
     git push -u origin "$1"
+
+    ssh -T $VM_HOST "wf_checkout_branch $1"
 }
 
